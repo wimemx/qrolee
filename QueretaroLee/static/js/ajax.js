0826906e275d35e72231b315547d7960098e7032
Fernando <soft.lfa@gmail.com>
2013-10-29 20:51:43 -0600

    Discussion fix

--- QueretaroLee/static/js/ajax.js
+++ QueretaroLee/static/js/ajax.js
@@ -899,7 +899,7 @@ function create_discussion(entity_id, name, content){
             });
             $span.find('a.title').trigger('click');
             if($('.discussion').find('.overview .item').length > 0)
-                $('.discussion').find('.overview .item').before($span);
+                $('.discussion').find('.overview .item:eq(0)').before($span);
             else{
                 $('.discussion').find('.overview p').before($span);
                 $('.discussion').find('.overview p').remove();
