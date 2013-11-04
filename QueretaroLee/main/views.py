fc3a337241baf001a55f4d5f405ae26659e27516
daniel <daniel@wime.com.mx>
2013-11-04 16:26:50 -0600

    img favicon

--- QueretaroLee/main/views.py
+++ QueretaroLee/main/views.py
@@ -179,7 +179,7 @@ def get_entities(request, **kwargs):
         return HttpResponse(context, mimetype='application/json')
     else:
         if entity_type == 'group':
-            entity_type = ['grupos', 'group']
+            entity_type = ['grupos de lectura', 'group']
         elif entity_type == 'spot':
             entity_type = ['lugares', 'spot']
         else:
@@ -187,7 +187,7 @@ def get_entities(request, **kwargs):
             entity_type = ['organizaciones', 'organization']
         content = 'Pellentesque habitant morbi tristique senectus et ' \
                   'netus et malesuada fames ac turpis egestas. Vestibulum ' \
-                  'tortor quam, feugiat vitae, ultricies eget, tempor sit ' \
+                  'tortor qprofileuam, feugiat vitae, ultricies eget, tempor sit ' \
                   'amet, ante. Donec eu libero sit amet quam egestas semper. ' \
                   'Aenean ultricies mi vitae est. Mauris placerat eleifend leo.' \
                   ' Quisque sit amet est et sapien ullamcorper pharetra. ' \
