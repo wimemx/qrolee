925d5beb4b73d34281206c61822522aba9ef9745
Fernando <soft.lfa@gmail.com>
2013-12-10 16:56:40 -0600

    Course null update

--- QueretaroLee/course/models.py
+++ QueretaroLee/course/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=255)
-    published = models.BooleanField()
+    name = models.CharField(max_length=255, null=True)
+    published = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
-    type = models.CharField(max_length=255)
-    type_pk = models.IntegerField(max_length=5)
-    description = models.TextField(max_length=2000,null=True)
+    type = models.CharField(max_length=255, null=True)
+    type_pk = models.IntegerField(max_length=5, null=True)
+    description = models.TextField(max_length=2000, null=True)
     status = models.BooleanField(default=True)
 
     def __unicode__(self):
@@ -18,38 +18,38 @@ class Course(models.Model):
 
 
 class Module(models.Model):
-    name = models.CharField(max_length=255)
-    text = models.TextField(max_length=2000)
+    name = models.CharField(max_length=255, null=True)
+    text = models.TextField(max_length=2000, null=True)
     order = models.IntegerField(max_length=5, default=0)
     date = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField(default=True)
-    course_dm = models.ForeignKey(Course)
+    course_dm = models.ForeignKey(Course, null=True)
 
     def __unicode__(self):
         return '%s, %s' % (self.name, self.order)
 
 
 class Content(models.Model):
-    name = models.CharField(max_length=255)
-    text = models.TextField(max_length=2000)
+    name = models.CharField(max_length=255, null=True)
+    text = models.TextField(max_length=2000, null=True)
     order = models.IntegerField(max_length=5, default=0)
     date = models.DateTimeField(auto_now_add=True)
-    status = models.BooleanField()
-    module_dm = models.ForeignKey(Module)
+    status = models.BooleanField(default=False)
+    module_dm = models.ForeignKey(Module, null=True)
 
     def __unicode__(self):
         return '%s, %s' % (self.name, self.order)
 
 
 class Test(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(max_length=2000)
-    status = models.BooleanField()
+    name = models.CharField(max_length=255, null=True)
+    description = models.TextField(max_length=2000, null=True)
+    status = models.BooleanField(default=False)
     date = models.DateTimeField(auto_now_add=True)
-    meta = models.CharField(max_length=255)
-    number_correct = models.IntegerField(blank=True)
-    type = models.BooleanField()
-    module_dm = models.ForeignKey(Module)
+    meta = models.CharField(max_length=255, null=True)
+    number_correct = models.IntegerField(blank=True, null=True)
+    type = models.BooleanField(default=False)
+    module_dm = models.ForeignKey(Module, null=True)
 
     def __unicode__(self):
         return '%s, %s' % (self.name, self.meta)
@@ -63,36 +63,36 @@ class Inscription(models.Model):
         return '%s, %s' % (self.course, self.user)
 
 
-class Question_Type(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.TextField(max_length=2000)
-    help_text = models.CharField(max_length=255)
-    meta = models.CharField(max_length=255)
-    date = models.DateTimeField(auto_now_add=True)
+#class Question_Type(models.Model):
+#    name = models.CharField(max_length=255)
+#    description = models.TextField(max_length=2000)
+#    help_text = models.CharField(max_length=255)
+#    meta = models.CharField(max_length=255)
+#    date = models.DateTimeField(auto_now_add=True)
 
-    def __unicode__(self):
-        return '%s, %s' % (self.name, self.help_text)
+#    def __unicode__(self):
+#        return '%s, %s' % (self.name, self.help_text)
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, null=True)
     meta = models.CharField(max_length=255, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    order = models.IntegerField(max_length=5)
-    type = models.ForeignKey(Question_Type)
-    test_dm = models.ForeignKey(Test)
+    order = models.IntegerField(max_length=5, null=True)
+    type = models.IntegerField(null=True)
+    test_dm = models.ForeignKey(Test, null=True)
 
     def __unicode__(self):
         return '%s, %s' % (self.title, self.order)
 
 
 class Option(models.Model):
-    label = models.CharField(max_length=255)
-    value = models.DecimalField(decimal_places=3, max_digits=3)
-    order = models.IntegerField(max_length=5)
-    meta = models.CharField(max_length=255)
+    label = models.CharField(max_length=255, null=True)
+    value = models.DecimalField(decimal_places=3, max_digits=3, null=True)
+    order = models.IntegerField(max_length=5, null=True)
+    meta = models.CharField(max_length=255, null=True)
     date = models.DateTimeField(auto_now_add=True)
-    question_dm = models.ForeignKey(Question)
+    question_dm = models.ForeignKey(Question, null=True)
 
     def __unicode__(self):
         return '%s, %s' % (self.label, self.question)
