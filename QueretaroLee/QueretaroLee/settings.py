7a56d92467057d5f4d14384017916e91dfa5087a
dennon001 <daniel@wime.com.mx>
2013-12-18 16:40:58 -0600

    settings

--- QueretaroLee/QueretaroLee/settings.py
+++ QueretaroLee/QueretaroLee/settings.py
@@ -17,11 +17,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',# Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'Queretaro_Lee',                      # Or path to database file if using sqlite3.
+        'NAME': 'queretaro_lee',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '8889',                      # Set to empty string for default. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -130,7 +130,6 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'account',
     'registry',
-    'disqus',
     'course'
 )
 
